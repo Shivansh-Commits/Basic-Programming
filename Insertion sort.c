f576b7190e14dea6a5444886239525b81b47a87d
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-02-17 12:38:54 +0530

    Update Insertion sort.c

--- Insertion sort.c
+++ Insertion sort.c
@@ -1,6 +1,6 @@
 #include<stdio.h> //Inclusion of Necessary Header File.\
 
-int insertion_sort(int a[],int n)// Definition of insertion Sort() function .
+int insertion_sort(int a[],int n)  // Definition of insertion Sort() function .
 {
     int j,t,i;
 for(i=1;i<n;i++)
@@ -15,7 +15,8 @@ for(i=1;i<n;i++)
     a[j+1]=t;
 }
 }
-void main()
+
+void main() 
 {
     int n=0,i=0;
     printf("ENTER THE SIZE OF ARRAY:");
@@ -26,7 +27,7 @@ void main()
     {
         scanf("%d",&a[i]);
     }
-    insertion_sort(a,n);
+    insertion_sort(a,n);  // Calling the Function insertion_sort() , which will implement Insertion sort on the Elements.
     for(i=0;i<n;i++)
     {
         printf("%d ,",a[i]);
