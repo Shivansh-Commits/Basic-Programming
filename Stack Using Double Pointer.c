84ffb79a7c07765b88777d120e0567126789dbd3
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-02-29 15:23:15 +0530

    Add files via upload

---
+++ Stack Using Double Pointer.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+    int info;
+    struct node_type *next;
+}node_type;
+void PUSH(node_type **tp)
+{
+    node_type *p;
+    int n;
+    p=(node_type*)malloc(sizeof(node_type));
+    if(p==NULL)
+    {
+        printf("NOT ENOUGH MEMORY");
+    }
+    else
+    {
+        printf("Enter the Value to be Inserted :");
+        scanf("%d",&n);
+        p->info=n;
+        p->next=*tp;
+        *tp=p;
+    }
+}
+void POP(node_type **tp)
+{
+    if(*tp==NULL)
+    {
+        printf("STACK IS EMPTY\n");
+    }
+    else
+    {
+       printf("ELEMENT POPED : %d\n",(*tp)->info);
+       node_type *temp;
+       temp=*tp;
+       (*tp)=(*tp)->next;
+       free(temp);
+    }
+}
+void Display(node_type *top)
+{
+while(top!=NULL)
+{
+    printf("%d ",top->info);
+    top=top->next;
+}
+}
+void main()
+{
+    char ch;
+    int n;
+    node_type *top=NULL;
+
+
+
+    do
+    {
+        printf("\n1.PUSH\n2.POP\n3.DISPLAY\n");
+        printf("\nEnter Your Choice : ");
+        scanf("%d",&n);
+        switch(n)
+        {
+            case 1:PUSH(&top);
+                   break;
+
+            case 2:POP(&top);
+                   break;
+
+            case 3:Display(top);
+                   break;
+        }
+
+        printf("\nDO YOU WANT TO CONTINUE (y/n)\n");
+        fflush(stdin);
+        scanf("%c",&ch);
+    }while(ch=='y');
+
+}
