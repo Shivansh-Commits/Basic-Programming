fca4fe34ef66e4e11cc612f2941a830b988f265d
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-03-03 23:04:37 +0530

    Update Sum of Array By Recusrion.c

--- Sum of Array By Recusrion.c
+++ Sum of Array By Recusrion.c
@@ -1,32 +1,35 @@
-#include<stdio.h>
+#include<stdio.h>// Inclusion of header Files
 #include<stdlib.h>
+
 int sum(int[],int);
+
 int main(){
 
 int i,n;
-
+    
 printf("Enter the Size of Array:");
 scanf("%d",&n);
+    
 int arr[n];
-
 printf("\nEnter the elements of Array:");
+    
 for(i=0;i<n;i++)
     scanf("%d",&arr[i]);
+    
 printf("Sum of Array: %d ",sum(arr,n-1));
 
-
 }
 
-int sum(int arr[],int n){
+int sum(int arr[],int n){    //Definition of Function Sum(int[],int)
 if(n==0)
-{
+    {
     printf("%d ,",arr[0]);
     return arr[0];
-}
+    }
 else
-{
+    {
     printf("%d ,",arr[n]);
     return arr[n]+sum(arr,n-1);
-}
+    }
 
 }
